71be9d8bd39d47c080bf81f8dd23c81b759a7d8f
abdurrahman4127 <71093608+abdurrahman4127@users.noreply.github.com>
2022-03-29 23:06:30 +0600

    got rid of her name

--- src/StaticMethode.java
+++ src/StaticMethode.java
@@ -1,5 +1,5 @@
-class TN {
-    static String name = "Thao Nhi";
+class AR {
+    static String name = "Abdur Rahman";
 
     static void name() {
         System.out.println(name);
@@ -7,17 +7,19 @@ class TN {
 }
 
 public class StaticMethode {
-    //java compiler calls using class name -> StaticMethode.main();
-    //if it wasn't 'static', we couldn't call it like 'StaticMethode.main();'
-    //to call non-static methods, we need to create object; but to call static methode, we need class
-    // if it wasn't static, we'd have to create an object of 'StaticMethode' (i.e. the public class)
-    // but then we'd be needing to create a constructor and pass parameters; but java compiler doesn't know it
+    /*
+     java compiler calls using class name -> StaticMethode.main();
+     if it wasn't 'static', we couldn't call it like 'StaticMethode.main();'
+     to call non-static methods, we need to create object; but to call static methode, we need class
+     if it wasn't static, we'd have to create an object of 'StaticMethode' (i.e. the public class)
+     but then we'd be needing to create a constructor and pass parameters; but java compiler doesn't know it
+    */
 
     public static void main(String[] args)
     {
-        TN tn = new TN();
+        AR obj = new AR();
 
-        System.out.println(TN.name);
-//        tn.name();
+        System.out.println(obj.name);
+//        obj.name();
     }
 }
